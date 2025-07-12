c1a167eb3c8e4f74bae879c80b79bec962e2ebaa
imlobster <zhizhilik@gmail.com>
2025-07-12 14:57:20 +0300

    First Working Serializer

---
+++ INIRIP/Coder.cs
@@ -0,0 +1,63 @@
+﻿using INIRIP.Analysis;
+using INIRIP.Compiler;
+
+namespace INIRIP
+{
+    public static class Coder
+    {
+        private static readonly Lexer lexer = new();
+        private static readonly Parser parser = new();
+
+        /// <summary>
+        /// Read values from ini.
+        /// </summary>
+        /// <param name="source">Your readed ini file</param>
+        /// <param name="outvals">Values deserialized from ini</param>
+        /// <returns>True if success. False if error</returns>
+        public static bool TryDecode
+        (
+            string source,
+
+            out Dictionary<
+                    ReadOnlyMemory<char>,
+                    Dictionary<
+                        ReadOnlyMemory<char>,
+                        ReadOnlyMemory<char>
+                    >
+            > outvals
+        )
+        {
+            if ( source == null ) { outvals = []; return false; }
+            if (!lexer.TryLexText(source.AsSpan(), out ReadOnlySpan<INIRIP.Models.Token> tokens)) { outvals = []; return false; }
+            if (!parser.TryParseTokens(source, tokens, out outvals)) { outvals = []; return false; }
+            return true;
+        }
+
+        /// <summary>
+        /// Save your values into ini.
+        /// </summary>
+        /// <param name="fileFullPath">Path in format 'C:\Users\Desktop\MyFile.ini'</param>
+        /// <param name="invalues">Values to be serialized</param>
+        /// <returns>True if success. False if error</returns>
+        public static bool TryEncode
+        (
+            string fileFullPath,
+
+            Dictionary<
+                    ReadOnlyMemory<char>,
+                    Dictionary<
+                        ReadOnlyMemory<char>,
+                        ReadOnlyMemory<char>
+                    >
+            > invalues
+        )
+        {
+            if(fileFullPath == null)
+            {
+                return false;
+            }
+
+            return FileExport.TryExportFile(invalues, fileFullPath);
+        }
+    }
+}
---
+++ INIRIP/Compiler/FileExport.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace INIRIP.Compiler
+{
+    public static class FileExport
+    {
+        private static readonly StringBuilder stringbuilder = new();
+
+        public static bool TryExportFile
+        (
+            Dictionary<
+                    ReadOnlyMemory<char>,
+                    Dictionary<
+                        ReadOnlyMemory<char>,
+                        ReadOnlyMemory<char>
+                    >
+            > invalues,
+
+            string filePathWithNameAndExtension
+        )
+        {
+            stringbuilder.Clear();
+            foreach (var kvp in invalues)
+            {
+                stringbuilder.Append('[');
+                stringbuilder.Append(kvp.Key);
+                stringbuilder.Append("]\r\n");
+                foreach(var kvp2 in kvp.Value)
+                {
+                    stringbuilder.Append(kvp2.Key);
+                    stringbuilder.Append('=');
+                    stringbuilder.Append(kvp2.Value);
+                    stringbuilder.Append("\r\n");
+                }
+            }
+
+            try
+            {
+                File.WriteAllText(
+                    filePathWithNameAndExtension,
+                    stringbuilder.ToString()
+                );
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
